9947ae251801bc641d883e05807a8436f4c0f6e5
Sosodope <sonadolkar93@hotmail.com>
2018-05-31 20:58:56 +0700

    Add React project files

---
+++ front-end/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './containers/index.css';
+import App from './containers/App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
