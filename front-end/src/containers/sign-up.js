5501df01619c4079d22135d329f8529c7d2f09ca
Ivan S <ivanms1@gmail.com>
2018-06-02 20:53:49 +0700

    added login and signup containers

--- front-end/src/containers/sign-up.js
+++ front-end/src/containers/sign-up.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class Signup extends Component {
+	constructor(props){
+      super(props)
+      this.state = { name: '', email: '', password: ''};
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e){
+      const target = e.target;
+      const value = target.value;
+      const name = target.name;
+
+      this.setState({[name]: value});
+    }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    alert('Welcome!');
+    this.setState({name: '', email: '', password: ''})
+  }
   render() {
     return (
-      <div>
-      	<h1>This is signup</h1>
-      </div>
-    );
+      <div className="signup-box">
+      		<div className="navbar"><p>Login</p></div>
+            <form onSubmit={this.handleSubmit} className="signup-form">
+              <input name="name" onChange={this.handleChange} value={this.state.name} placeholder="Name" type="text"/>
+              <input name="email" onChange={this.handleChange} value={this.state.email} placeholder="Email" type="text"/>
+              <input name="password" onChange={this.handleChange} value={this.state.password} placeholder="Password" type="text"/>
+              <input type="submit"/>
+            </form>
+        </div>    );
   }
 }
 
