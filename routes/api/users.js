c2ed4e6ebd26da341bcf05cced381c0e93b8b0d8
Ben S <bsadick@gmail.com>
2018-06-03 19:52:56 +0900

    backend initially configured

---
+++ routes/api/users.js
@@ -0,0 +1,130 @@
+// this as auth, handles logging in, pw stuff etc
+const express = require("express");
+const router = express.Router();
+const gravatar = require("gravatar");
+
+//encryption for password
+const bcrypt = require("bcryptjs");
+//Load user model
+const User = require("../../models/User");
+
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
+
+const passport = require("passport");
+//below refers to /api/users/test
+
+//@route  GET api/users/test
+//@desc   Tests users route
+//@access Public
+router.get("/test", (req, res) => res.json({ msg: "Users works" }));
+
+/* @route  GET api/users/register
+   @desc   Register a User
+   @access Public  
+*/
+
+router.post("/register", (req, res) => {
+  let errors = {};
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      errors.email = "Email already exists";
+      return res.status(400).json({ errors });
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: "200", //size
+        r: "pg", //rating
+        d: "mm" //default
+      });
+
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) {
+            throw err;
+          } else {
+            newUser.password = hash;
+            newUser
+              .save()
+              .then(user => res.json(user))
+              .catch(err => console.log(err));
+          }
+        });
+      });
+    }
+  });
+});
+
+/* @route  GET api/users/login
+   @desc   Login a User / returning JWT
+   @access Public  
+*/
+
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  let errors = {};
+  //Find user by email
+  User.findOne({
+    email
+  }).then(user => {
+    //check for user
+    if (!user) {
+      errors.email = "User not found";
+      return res.status(404).json({ errors });
+    }
+    //Check password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        //pw/user is a match
+
+        // create JWT payload
+        const payload = {
+          id: user.id,
+          name: user.name,
+          avatar: user.avatar
+        };
+        //sign token
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            //issue a token here
+            res.json({
+              success: true,
+              token: "Bearer " + token
+            });
+          }
+        );
+      } else {
+        errors.password = "Password incorrect";
+        return res.status(400).json(errors);
+      }
+    });
+  });
+});
+
+//@route  GET api/users/current
+//@desc   return current user
+//@access Private
+
+router.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    });
+  }
+);
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const users = require("./routes/api/users");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const logger = require("morgan");
+
+const app = express();
+
+//setup middleware to get req.body
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//log activity in console with morgan
+app.use(logger("dev"));
+
+//db config
+const db = require("./config/keys").mongoURI;
+mongoose
+  .connect(db)
+  .then(() => console.log("Connected to Mongo"))
+  .catch(err => console.log(err));
+
+//init and configure passport
+app.use(passport.initialize());
+require("./config/passport")(passport);
+
+// app.use("/api/users", users);
+
+// use routes
+app.use("/api/users", users);
+
+const port = process.env.PORT || 10000;
+
+app.listen(10000, () => {
+  console.log(`Listening on ${port}`);
+});
