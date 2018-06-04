a27adc16aaa11d507faa1dc63b5a22b48c730a8c
Sosodope <sonadolkar93@hotmail.com>
2018-06-04 23:29:35 +0700

    Add banner to home page

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -3,17 +3,43 @@ import { Link } from "react-router-dom";
 import logo from "../images/LeetUplogo.png";
 import "../containers/index.css";
 
-export default function Header(props) {
-  return (
-    <header>
-      <Link to={"/"} className="brand">
-        <img src={logo} className="logo" alt="logo" />
-        <h1>{props.title}</h1>
-      </Link>
-      <nav>
-        <Link to={"/login"}>{props.login}</Link>
-        <Link to={"/signup"}>{props.signup}</Link>
-      </nav>
-    </header>
-  );
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: "logged-off"
+    };
+    this.logIn = this.logIn.bind(this);
+    this.logOff = this.logOff.bind(this);
+  }
+  logIn = () => {
+    this.setState((currentState, props) => {
+      return {
+        status: "logged-on"
+      };
+    });
+  };
+  logOff = () => {
+    this.setState((currentState, props) => {
+      return {
+        status: "logged-off"
+      };
+    });
+  };
+  render() {
+    return (
+      <header>
+        <Link to={"/"} className="brand">
+          <img src={logo} className="logo" alt="logo" />
+          <h1>{this.props.title}</h1>
+        </Link>
+        <nav>
+          <Link to={"/login"}>{this.props.login}</Link>
+          <Link to={"/signup"}>{this.props.signup}</Link>
+        </nav>
+      </header>
+    );
+  }
 }
+
+export default Header;
