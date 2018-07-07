7fa64a1f615daa35b5d0252f8cba944a5031a24c
Sosodope <sonadolkar93@hotmail.com>
2018-07-07 20:12:37 +0700

    Add form inputs for profile editing

--- front-end/src/containers/Account.js
+++ front-end/src/containers/Account.js
@@ -1,34 +1,35 @@
 import React from "react";
 import { MyContext } from "./context.js";
-import { Button } from "react-materialize";
+import { Row, Input, Button } from "react-materialize";
 import logo from "../images/LeetUplogo.png";
 import "./index.css";
 
 const Account = () => (
   <MyContext.Consumer>
-    {({ addBio, updateAvatar, state }) => (
-      <div className="user-profile wrapper">
-        <div className="user-info">
-          <h1>{state.name}</h1>
-        </div>
-        <div className="img-container">
-          <img src={logo} alt="" />
-          <Button className="addBio" waves="light" onClick={updateAvatar}>
-            Update Avatar
-          </Button>
-        </div>
-        <div className="interests">
-          <h3>Interests</h3>
-          <form action="">
-            <textarea>Interests, Interests, Interests</textarea>
-            <input type="submit" value="Update Interests" />
-          </form>
-        </div>
-        <div className="user-bio">
-          <p>{state.bio ? state.bio : ""}</p>
-          <Button className="addBio" waves="light" onClick={addBio}>
-            Add Bio
-          </Button>
+    {({ state }) => (
+      <div className="wrapper">
+        <div className="">
+          <h5>Account Settings</h5>
+          <Row>
+            <p>{state.name}</p>
+            <Input
+              s={6}
+              label="Full Name"
+              validate
+              placholder="Your Full Name"
+            />
+            <Input s={6} label="Email" validate placholder="Your Email" />
+            <Input s={6} label="Location" validate placholder="Your Location" />
+            <Input
+              s={6}
+              label="Membership Date"
+              validate
+              placholder="Member Since"
+            />
+            <Input label="Interests" type="textarea" />
+            <Input label="Bio" type="textarea" />
+            <Input label="Photo" type="file" />
+          </Row>
         </div>
       </div>
     )}
