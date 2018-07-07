6fa427b4af65e4a6175cf7880e5ef8e072767558
Sosodope <sonadolkar93@hotmail.com>
2018-07-07 20:19:15 +0700

    Add default values from state

--- front-end/src/containers/Account.js
+++ front-end/src/containers/Account.js
@@ -11,24 +11,35 @@ const Account = () => (
         <div className="">
           <h5>Account Settings</h5>
           <Row>
-            <p>{state.name}</p>
             <Input
-              s={6}
+              s={12}
               label="Full Name"
               validate
               placholder="Your Full Name"
+              defaultValue={state.name}
             />
-            <Input s={6} label="Email" validate placholder="Your Email" />
-            <Input s={6} label="Location" validate placholder="Your Location" />
+            <Input s={12} label="Email" validate placholder="Your Email" />
             <Input
-              s={6}
-              label="Membership Date"
+              s={12}
+              label="Location"
               validate
-              placholder="Member Since"
+              placholder="Your Location"
             />
-            <Input label="Interests" type="textarea" />
-            <Input label="Bio" type="textarea" />
-            <Input label="Photo" type="file" />
+            <Input
+              type="password"
+              s={12}
+              label="Password"
+              validate
+              placholder="Your Password"
+            />
+            <Input s={12} label="Interests" type="textarea" />
+            <Input
+              s={12}
+              label="Bio"
+              defaultValue={state.bio}
+              type="textarea"
+            />
+            <Input s={12} label="Photo" type="file" />
           </Row>
         </div>
       </div>
