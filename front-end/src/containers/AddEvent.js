d0ca5c4419af834e3e24ff976f9b7c75e6425ad1
Sosodope <sonadolkar93@hotmail.com>
2018-07-17 23:12:31 +0700

    Set up event creation form

--- front-end/src/containers/AddEvent.js
+++ front-end/src/containers/AddEvent.js
@@ -2,12 +2,35 @@ import React from "react";
 import { MyContext } from "./context.js";
 import logo from "../images/LeetUplogo.png";
 import "./index.css";
+import { Input } from "react-materialize";
 
 const AddEvent = () => (
   <MyContext.Consumer>
     {({ state }) => (
       <div className="wrapper">
-        <h5>Create a new Event</h5>
+        <div>
+          <h5>Create an Event</h5>
+        </div>
+        <form>
+          <input name="eventTitle" placeholder="Add Event Title" type="text" />
+          <Input
+            name="on"
+            placeholder="Add Event Date"
+            type="date"
+            onChange={function(e, value) {}}
+          />
+          <input name="eventTime" placeholder="Add Event Time" type="text" />
+          <input
+            name="eventLocation"
+            placeholder="Add Event Location"
+            type="text"
+          />
+          <textarea
+            name="eventDescription"
+            placeholder="Add Event Description"
+          />
+          <input type="submit" className="btn" value="Create Event" />
+        </form>
       </div>
     )}
   </MyContext.Consumer>
