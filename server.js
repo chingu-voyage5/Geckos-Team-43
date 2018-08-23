f4d018328932e95044b5aaeb6cd11942178e63a4
Ivan S <ivanms1@gmail.com>
2018-08-23 21:14:10 +0700

    ready for deployment

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const profile = require('./routes/api/profile');
 const events = require('./routes/api/events');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 const logger = require('morgan');
 const cors = require('cors');
 const app = express();
@@ -36,6 +37,15 @@ require('./config/passport')(passport);
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/events', events);
+
+//Serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+	app.use(express.static('front-end/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'front-end', 'build', 'index.html'));
+	})
+}
 
 const port = process.env.PORT || 10000;
 
